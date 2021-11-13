db56817585f595a127209d00389264dd6c20878a
Sumit Dey <sumit@betterhalf.ai>
2021-11-13 16:01:19 +0530

    files added

---
+++ src/components/SplashScreen.tsx
@@ -0,0 +1,52 @@
+import { useMediaQuery } from "@react-hook/media-query";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+const SplashScreen = () => {
+  const smallScreen = useMediaQuery("only screen and (min-width: 748px)");
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  return (
+    mounted && (
+      <motion.div
+        className="fixed z-10 w-full h-screen"
+        initial={{
+          y: 0,
+        }}
+        animate={{
+          y: "-150%",
+          transition: {
+            duration: 1.6,
+          },
+        }}
+      >
+        {/* {!smallScreen && (
+        <svg viewBox="0 0 360 675" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M360 675C240 600 120 600 0 675V0H360V675Z" fill="white" />
+        </svg>
+      )} */}
+
+        <svg viewBox={smallScreen ? "0 0 1248 941" : "0 0 360 675"} fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d={
+              smallScreen
+                ? "M1248 941C832 836.444 416 836.444 0 941V0H1248V941Z"
+                : "M360 675C240 600 120 600 0 675V0H360V675Z"
+            }
+            fill="white"
+          />
+        </svg>
+
+        {/* {smallScreen && (
+          <svg viewBox="0 0 1248 941" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1248 941C832 836.444 416 836.444 0 941V0H1248V941Z" fill="white" />
+          </svg>
+        )} */}
+      </motion.div>
+    )
+  );
+};
+
+export default SplashScreen;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,81 @@
-import Delete from "@components/Delete";
+// @ts-nocheck
+
+import Image from "next/image";
+import blusky from "@public/blusky.jpg";
+import dec from "@public/dec2021.jpg";
+import sudo from "@public/sudo.jpg";
+import SplashScreen from "@components/SplashScreen";
+import Head from "next/head";
+import { Fireworks } from "fireworks-js/dist/react";
+import { FireworksOptions } from "fireworks-js";
+import Confetti from "@components/Confetti";
 
 export default function Home() {
-  return <Delete />;
+  const options: FireworksOptions = {
+    speed: 3,
+    sound: {
+      enabled: true,
+      files: ["explosion0.mp3", "explosion1.mp3", "explosion2.mp3"],
+      volume: {
+        min: 1,
+        max: 40,
+      },
+    },
+  };
+
+  const style = {
+    left: 0,
+    top: 0,
+    width: "100%",
+    height: "100%",
+    position: "fixed",
+    background: "transparent",
+  };
+  return (
+    <>
+      <Head>
+        {/* // TODO : add title */}
+        <title> </title>
+      </Head>
+      <SplashScreen />
+      <div className="flex flex-col items-center justify-center bg-[#FE93D1]  ">
+        <div className="w-[100vw] h-[79vh] sm:h-[100vh] relative ">
+          <Image
+            src={blusky}
+            alt=""
+            layout="fill"
+            objectFit="contain"
+            quality={100}
+            className="shadow-lg"
+            placeholder="blur"
+          />
+        </div>
+        <div className="w-[100vw] h-[79vh] sm:h-[100vh] relative nextImage">
+          <Image
+            src={sudo}
+            alt=""
+            layout="fill"
+            objectFit="contain"
+            quality={100}
+            className="shadow-lg"
+            placeholder="blur"
+          />
+        </div>
+        <div className="w-[100vw] h-[79vh] sm:h-[100vh] relative ">
+          <Image
+            src={dec}
+            alt=""
+            layout="fill"
+            objectFit="contain"
+            quality={100}
+            className="shadow-lg"
+            placeholder="blur"
+          />
+        </div>
+      </div>
+
+      <Fireworks options={options} style={style} />
+      <Confetti />
+    </>
+  );
 }
